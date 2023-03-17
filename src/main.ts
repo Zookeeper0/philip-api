1c4beb1fe96530584c7af38fa01c6f6322b4a8db
somehow <snsdkf6@gmail.com>
2023-03-17 16:25:16 +0900

    [gaeun] SSL key pare 적용

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import * as fs from "fs";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync(`${process.env.SSL_KEY_PATH}`, "utf8"),
+    cert: fs.readFileSync(`${process.env.SSL_CRT_PATH}`, "utf8"),
+  };
+
+  const app = await NestFactory.create(AppModule, { httpsOptions });
+
   app.enableCors({
     // origin: 'http://localhost:3001/',
     // allowedHeaders: ['content-type'],
