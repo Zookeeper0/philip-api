c853d56b6072513d961470f5d028602e5144cce9
somehow <snsdkf6@gmail.com>
2023-03-21 17:10:12 +0900

    [gaeun] CORS 열음

--- src/main.ts
+++ src/main.ts
@@ -12,13 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { httpsOptions });
   // const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    // origin: 'http://localhost:3001/',
-    // allowedHeaders: ['content-type'],
-    origin: true,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-    credentials: true,
-  });
+  app.enableCors();
   app.use(cookieParser());
   await app.listen(3042);
 }
