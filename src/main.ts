0c9642a94827cd116782c91c349b902163107841
somehow <snsdkf6@gmail.com>
2023-03-22 17:24:14 +0900

    [gaeun] CORS Config 변경

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,21 @@ import * as fs from "fs";
 import * as cookieParser from "cookie-parser";
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync(`${process.env.SSL_KEY_PATH}`, "utf8"),
-    cert: fs.readFileSync(`${process.env.SSL_CRT_PATH}`, "utf8"),
-  };
+  // const httpsOptions = {
+  //   key: fs.readFileSync(`${process.env.SSL_KEY_PATH}`, "utf8"),
+  //   cert: fs.readFileSync(`${process.env.SSL_CRT_PATH}`, "utf8"),
+  // };
 
-  const app = await NestFactory.create(AppModule, { httpsOptions });
-  // const app = await NestFactory.create(AppModule);
+  // const app = await NestFactory.create(AppModule, { httpsOptions });
+  const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    // origin: 'http://localhost:3001/',
+    // allowedHeaders: ['content-type'],
+    origin: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    credentials: true,
+  });
   app.use(cookieParser());
   await app.listen(3042);
 }
