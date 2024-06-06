5b02478dcf9419fe92aa8ed7bdc848712c2cb485
안길우 <gilu91@insystem.kr>
2024-06-06 12:43:46 +0900

    UPDATE: 코드에서 ssl 등록하는 부분 삭제

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,9 @@ import * as fs from "fs";
 import * as cookieParser from "cookie-parser";
 
 async function bootstrap() {
-  let app: any;
-
-  if (process.env.NODE_ENV === "development") {
-    app = await NestFactory.create(AppModule);
-  } else {
-    const httpsOptions = {
-      key: fs.readFileSync(`${process.env.SSL_KEY_PATH}`, "utf8"),
-      cert: fs.readFileSync(`${process.env.SSL_CRT_PATH}`, "utf8"),
-    };
-
-    app = await NestFactory.create(AppModule, { httpsOptions });
-  }
+  const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    // origin: 'http://localhost:3001/',
-    // allowedHeaders: ['content-type'],
     origin: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
     credentials: true,
